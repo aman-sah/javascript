e21264690d21d3d6c9c4c4f285e16b947b5b3782
Aman <125241580+aman-sah@users.noreply.github.com>
2024-05-27 20:02:43 +0000

    conversions of data type

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score="aman"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber=Number(score);
+
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+//"33"=>33(Number)
+//"33abc"=>NAN
+//true=>1,False=>0
+
+
+let isLoggedIn="Aman";
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+//1=>true,2=>false
+//""=>false
+//"aman"=>true
+
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
