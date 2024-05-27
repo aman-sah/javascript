a1c585219b8032f2a5ad5cc2ea3af92b51268ce7
Aman <125241580+aman-sah@users.noreply.github.com>
2024-05-27 19:43:50 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict"  //treat all js code as newer version
+
+// alert(1+1) // we are using nodejs ,not browser
+
+
+console.log(3 
+    +3) //code readablity should be high
+
+console.log("Aman")    
+
+let name="Aman"
+let age=18
+let isLoggedIn=false;
+let state
+
+
+//number =>2 to power 53
+//bigInt
+//string=""
+//boolean =true/false
+//null =>standalone value
+//undefined
+//symbol=>unique
+
+
+//object
+
+
+console.log(typeof "Aman");
+console.log(typeof null); //object
+console.log(typeof undefined);//undefined
+
+
+
