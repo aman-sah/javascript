9ca54552823715f6d7dce6f3740f8c32291bf57b
Aman <125241580+aman-sah@users.noreply.github.com>
2024-05-31 07:51:25 +0000

    datatype summary

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,54 @@
+//Primitive
+
+//7 types: String, Number,Boolean ,null, undefined ,Symbol(to make unique any value),BigInt
+
+// Primitive datatypes:
+// String - string
+// Number - number
+// Boolean - Boolean
+// Null - object(****)
+// Undefined - undefined
+// BigInt - bigint
+
+const score=100;
+const scoreValue=100.3
+
+const isLoggedIn=false
+
+const outsideTemp=null
+
+let userEmail;
+
+const id1=Symbol('123');
+const id2=Symbol('123');
+// console.log(id1===id2);
+
+const bigNumber=74786782636568738n
+
+
+
+//Reference(Non Primitive)
+
+//Array, Object, Functions
+
+// Non-primitive datatypes:
+// Array - object
+// Function - function(object)
+// Object - object
+
+const heroes=["shaktiman","krrish","nnagraj","doga"];
+
+let myObj={
+    name:"aman",
+    age:21,
+}
+
+const myFunction=function(){
+    console.log("hello");
+}
+console.log(typeof myFunction);
+
+
+
+
+
