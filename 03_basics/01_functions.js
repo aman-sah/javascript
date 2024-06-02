fdcb95145d42bb9815d5517ccd70923a5a39fc66
aman-sah <amankumarmdp88@gmail.com>
2024-06-02 13:00:10 +0530

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -25,4 +25,33 @@ function loginUserMessage(username="sam"){
     
 }
 // console.log(loginUserMessage("aman"));
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+
+
+function calculateCartPrice(val1,val2,...num1){   // (...num1)rest operator
+    return num1
+}
+// console.log(calculateCartPrice(200,300,400,1000));
+
+const user={
+    username:"abc",
+    price:199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+// handleObject(user);
+handleObject({
+    username:"sam",
+    price:399
+})
+
+const mynewArray=[200,400,100,600]
+
+function returnSecondaryValue(getArray){
+    return getArray[1]
+}
+// console.log(returnSecondaryValue(mynewArray));
+console.log(returnSecondaryValue([200,400,500,1000]));
